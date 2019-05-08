e073b0ba337b1ae544c16ddcbb93b9a7c15126c1
habumugisha <habajeunes2@gmail.com>
2019-05-08 17:01:29 +0200

    bg as developer i should arrange pages and images in folders

--- public/navbar.js
+++ public/navbar.js
@@ -1,7 +1,6 @@
 const navSlide = () =>{
     const burger = document.querySelector('.burger');
     const top = document.querySelector('.menu');
-    
 
     burger.addEventListener('click',() =>{
         top.classList.toggle('menu-active');
